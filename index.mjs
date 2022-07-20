6f7bbf163894baa71c6cad647306007155096213
Kamran Akhmedov <romanticist77@gmail.com>
2022-07-20 09:05:56 +0100

    removed commented lines and changed css path

--- index.mjs
+++ index.mjs
@@ -3,22 +3,11 @@ import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-d
 dotenv.config()
 import express from "express"
 import mongoose from 'mongoose'
-import { MongoClient, ServerApiVersion } from 'mongodb'
 import methodOverride from 'method-override'
 import flash from 'connect-flash'
 import {router} from './config/routes.js'
 import {setupAssets} from './config/assets.js'
 
-// const uri = "mongodb+srv://achmedow:Z7gk4YJHQUre7OKA@cluster0.lluxj.mongodb.net/?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   console.log(process.env.MONGO_URI)
-//   console.log(process.env.uri)
-  
-//   // perform actions on the collection object
-//   client.close();
-// });
 await mongoose.connect("mongodb+srv://achmedow:Z7gk4YJHQUre7OKA@cluster0.lluxj.mongodb.net/?retryWrites=true&w=majority")
 
 const PORT = process.env.PORT || 3000
